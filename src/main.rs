c4e4763fcbba4d7a1240e5815f141ea4a7f41456
“vitalvirtue” <hyterdem@hotmail.com>
2024-02-03 12:47:24 +0300

    implementation WordCounter with new, read_text and count_wrods

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,24 @@ struct WordCounter {
     text: String,
 }
 
+impl WordCounter {
+    //Constructor
+    fn new() -> WordCounter {
+        WordCounter {
+            text: String::new(),
+        }
+    }
 
+    //Reading texts
+    fn read_text(&mut self) {
+        println!("Enter some text:");
+        io::stdin()
+            .read_line(&mut self.text)
+            .expect("Failed to read line");
+    }
+
+    //Counting words
+    fn count_words(&self) -> usize {
+        self.text.split_whitespace().count()
+    }
+}
